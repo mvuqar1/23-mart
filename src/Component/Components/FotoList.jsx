7ff77f45fc48bbabe7f112e608312bd0c576cadc
unknown <vuqar.5100@mail.ru>
2023-03-27 17:56:45 +0400

    fgh

--- src/Component/Components/FotoList.jsx
+++ src/Component/Components/FotoList.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import {useEffect} from 'react';
 
 import { useNavigate } from 'react-router-dom'
 
@@ -9,9 +10,26 @@ export default function FotoItem(props) {
 
   const navigate = useNavigate()
   const goBack = () => navigate(-1)
+  const keyButton=(e)=>{
+    console.log(e.target);
+  }
+  useEffect(() => {
+    const keyDownHandler = event => {
+      // console.log('User pressed: ', event.key);
 
+      if (event.key === 'Backspace') {
+        event.preventDefault();
+        goBack();
+      }
+    };
+    document.addEventListener('keydown', keyDownHandler);
+
+    return () => {
+      document.removeEventListener('keydown', keyDownHandler);
+    };
+  }, []);
   return (
-    <div>
+    <div onKeyUp={keyButton}>
       <button className='btn-1' onClick={goBack}>Go Back</button>
       <div>{fotoId.title}</div>
       <img style={{ width: "300px" }} src={fotoId.image} alt={fotoId.title} />
